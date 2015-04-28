376185a6dbb8509763c7c5a0a14a838b64489ed2
Anton Yeshchenko <mazepa007@gmail.com>
2015-04-28 11:52:08 +0300

    add output for light seosor in lux

--- app/src/main/java/example/com/yourlightmetered/main_view.java
+++ app/src/main/java/example/com/yourlightmetered/main_view.java
@@ -2,34 +2,61 @@ package example.com.yourlightmetered;
 
 import android.app.Activity;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 
-public class main_view extends Activity {
+public class main_view extends Activity implements SensorEventListener {
 
-    private SensorManager sensorManager;
+    private SensorManager mSensorManager;
+    private Sensor mLightSensor;
 
-    private TextView textView;
+    private TextView mTextView, mTextView2;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_view);
-        textView = (TextView)findViewById(R.id.textView);
+        mTextView = (TextView)findViewById(R.id.textView);
+        mTextView2 = (TextView)findViewById(R.id.textView2);
 
 
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
 
-        sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-
-
-        textView.setText(sensorManager.getSensorList(Sensor.TYPE_LIGHT).toString());
+        mTextView.setText(mSensorManager.getSensorList(Sensor.TYPE_LIGHT).toString());
     }
 
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mLightSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.e("asdasd", "light accuracy");
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Log.e("asdasd", "light changed");
+        mTextView2.setText("Lux: " + String.valueOf(event.values[0]));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
