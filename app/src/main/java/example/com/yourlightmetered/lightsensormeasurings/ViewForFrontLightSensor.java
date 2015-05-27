2a7144bb4e797ccc9437fcf651283b7c7ceca749
Anton Yeshchenko <mazepa007@gmail.com>
2015-05-27 11:41:59 +0300

    add code for buttons

--- app/src/main/java/example/com/yourlightmetered/lightsensormeasurings/ViewForFrontLightSensor.java
+++ app/src/main/java/example/com/yourlightmetered/lightsensormeasurings/ViewForFrontLightSensor.java
@@ -9,7 +9,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import example.com.yourlightmetered.Converter;
 import example.com.yourlightmetered.R;
@@ -22,6 +26,30 @@ public class ViewForFrontLightSensor extends Activity implements SensorEventList
 
     private TextView mTextView, mTextView2;
 
+    private ArrayList<Integer> ISO =
+             new ArrayList<>(Arrays.asList(50,64,80,100,125,160,200,250
+                     ,320,400,500,640,800,1000,1250,1600));
+    private ArrayList<Double> Fstop = new ArrayList<Double>(Arrays.asList(
+            1., 1.4, 1.6, 1.7, 1.8, 2., 2.2, 2.4, 2.5, 2.8, 3.2, 3.3, 3.5, 4.,
+            4.5, 4.8, 5.0, 5.5, 5.6, 6.3, 6.7, 7.1, 8., 9.,
+            9.5, 10., 11., 13., 14., 16., 18., 19., 20.));
+
+    // 1 / k for shutter speed
+
+    private ArrayList<Double> shutterSpeed = new ArrayList<>(Arrays.asList(
+        60.,30.,15.,13.,12.,10.,8.,6.,5.,4.,3.,2.5,2.,1.6,1.5,1.3,1.
+    ));
+
+
+    /* here should be or
+        0 - f stop
+        1 - sec
+        2  - iso
+    */
+    int whatWeCalculateFor = 0;
+
+    Button mButtonFstop, mButtonSec, mButtonISO, mButtonStopPreview;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +59,13 @@ public class ViewForFrontLightSensor extends Activity implements SensorEventList
         mTextView2 = (TextView)findViewById(R.id.textView2);
 
 
-        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mButtonFstop = (Button)findViewById(R.id.button_f);
+        mButtonISO = (Button)findViewById(R.id.button_iso);
+        mButtonSec = (Button)findViewById(R.id.button_sec);
+        mButtonStopPreview = (Button)findViewById(R.id.button_stop_measurings);
+
+
+                mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mLightSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
 
         mTextView.setText(mSensorManager.getSensorList(Sensor.TYPE_LIGHT).toString());
@@ -49,6 +83,11 @@ public class ViewForFrontLightSensor extends Activity implements SensorEventList
         super.onPause();
         mSensorManager.unregisterListener(this);
     }
+
+    void calculateForFstop(double EV){
+
+    }
+
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
